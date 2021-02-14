cd404af2687f356000767c30530843b3371fee71
shina <rasheedshinaopeyemi@gmail.com>
2021-02-14 11:52:24 +0100

    added all the homepage sections

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,6 @@
+import React from "react"
+
+// services
 import service1 from "../assets/img/rr1.png"
 import service2 from "../assets/img/supply1.png"
 import service3 from "../assets/img/Platform_Supply_Vessel_.jpg"
@@ -5,7 +8,53 @@ import service4 from "../assets/img/crewsuppy.jpg"
 import service5 from "../assets/img/securityvessel.jpg"
 import service6 from "../assets/img/barges.jpg"
 
-import { FaRss } from "react-icons"
+// selling poinst
+import { FaRss } from "react-icons/fa"
+import { FaShieldAlt } from "react-icons/fa"
+import { FaUserSecret } from "react-icons/fa"
+import { FaCogs } from "react-icons/fa"
+import { FaCheck } from "react-icons/fa"
+import { FaHandshake } from "react-icons/fa"
+import { FaMoneyBillAlt } from "react-icons/fa"
+import { FaUsers } from "react-icons/fa"
+import { FaPaperPlane } from "react-icons/fa"
+import { FaStreetView } from "react-icons/fa"
+
+//certification and licenses
+import cert1 from "../assets/img/cert1.png"
+import cert2 from "../assets/img/cert2.png"
+import cert3 from "../assets/img/cert3.png"
+import cert4 from "../assets/img/cert4.png"
+import cert5 from "../assets/img/cert5.png"
+import cert6 from "../assets/img/cert6.png"
+import cert7 from "../assets/img/cert7.png"
+import cert8 from "../assets/img/cert8.png"
+import cert9 from "../assets/img/cert9.png"
+import cert10 from "../assets/img/cert10.png"
+import cert11 from "../assets/img/cert11.png"
+
+//clients
+import client1 from "../assets/img/client1.png"
+import client2 from "../assets/img/client2.png"
+import client3 from "../assets/img/client3.png"
+import client4 from "../assets/img/client4.png"
+import client5 from "../assets/img/client5.png"
+import client6 from "../assets/img/client6.png"
+import client7 from "../assets/img/client7.png"
+import client8 from "../assets/img/client8.png"
+import client9 from "../assets/img/client9.png"
+import client10 from "../assets/img/client10.png"
+import client11 from "../assets/img/client11.png"
+
+//gallery
+import gallery1 from "../assets/img/ga1.png"
+import gallery2 from "../assets/img/ga2.jpg"
+import gallery3 from "../assets/img/ga3.png"
+import gallery4 from "../assets/img/ga4.jpg"
+import gallery5 from "../assets/img/ga5.jpg"
+import gallery6 from "../assets/img/gallery1.jpg"
+import gallery7 from "../assets/img/ga6.jpg"
+import gallery8 from "../assets/img/ga7.png"
 
 export const servicesData = [
   {
@@ -38,4 +87,146 @@ export const servicesData = [
   },
 ]
 
-export const sellingPoints = [{}]
+export const sellingPoints1 = [
+  {
+    icon: <FaRss />,
+    text: "Reliability",
+  },
+  {
+    icon: <FaShieldAlt />,
+    text: "High Safety Standards",
+  },
+  {
+    icon: <FaUserSecret />,
+    text: "Professionalism",
+  },
+  {
+    icon: <FaCogs />,
+    text: "Proficient Technical Know-How",
+  },
+  {
+    icon: <FaCheck />,
+    text: "Service Driven",
+  },
+]
+
+export const sellingPoints2 = [
+  {
+    icon: <FaHandshake />,
+    text: "Honesty And Sincerity",
+  },
+  {
+    icon: <FaMoneyBillAlt />,
+    text: "Robust Financial Strength",
+  },
+  {
+    icon: <FaUsers />,
+    text: "High Efficiency Level",
+  },
+  {
+    icon: <FaPaperPlane />,
+    text: " Result Oriented",
+  },
+  {
+    icon: <FaStreetView />,
+    text: "Ecologically Friendly Services",
+  },
+]
+export const certification = [
+  {
+    certification: cert1,
+  },
+  {
+    certification: cert2,
+  },
+  {
+    certification: cert3,
+  },
+  {
+    certification: cert4,
+  },
+  {
+    certification: cert5,
+  },
+  {
+    certification: cert6,
+  },
+  {
+    certification: cert7,
+  },
+  {
+    certification: cert8,
+  },
+  {
+    certification: cert9,
+  },
+  {
+    certification: cert10,
+  },
+  {
+    certification: cert11,
+  },
+]
+
+export const clients = [
+  {
+    client: client1,
+  },
+  {
+    client: client2,
+  },
+  {
+    client: client3,
+  },
+  {
+    client: client4,
+  },
+  {
+    client: client5,
+  },
+  {
+    client: client6,
+  },
+  {
+    client: client7,
+  },
+  {
+    client: client8,
+  },
+  {
+    client: client9,
+  },
+  {
+    client: client10,
+  },
+  {
+    client: client11,
+  },
+]
+
+export const gallery = [
+  {
+    gallery: gallery1,
+  },
+  {
+    gallery: gallery2,
+  },
+  {
+    gallery: gallery3,
+  },
+  {
+    gallery: gallery4,
+  },
+  {
+    gallery: gallery5,
+  },
+  {
+    gallery: gallery6,
+  },
+  {
+    gallery: gallery7,
+  },
+  {
+    gallery: gallery8,
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,14 @@ import "../styles/index.scss"
 import Layout from "../components/layout"
 import IndexHero from "../components/Home/Hero"
 import serviceImg from "../assets/img/circle.jpg"
-import { servicesData } from "../data/index"
+import {
+  servicesData,
+  sellingPoints1,
+  sellingPoints2,
+  certification,
+  clients,
+  gallery,
+} from "../data/index"
 
 const index = () => {
   return (
@@ -44,21 +51,142 @@ const index = () => {
           </h5>
         </div>
         <div className="servicesImages">
-          {servicesData.map(service => (
-            <div className="service">
+          {servicesData.map((service, index) => (
+            <div key={index} className="service">
               <img src={service.img} alt="service" />
             </div>
           ))}
         </div>
+        <Link className="text-center" to="/">
+          Read More
+        </Link>
       </section>
       <section className="indexFourthSection">
-        <div>
-          <h5>NICHE SERVICES</h5>
-          <p>
-            At SR Platforms, we provide tools for <br /> professional services
-            delivery and operational <br /> excellence that ensures our client
-            satisfaction <br /> and value for our shareholders.
-          </p>
+        <article className="description">
+          <div className="nicheServices">
+            <h5>NICHE SERVICES</h5>
+            <p>
+              At SR Platforms, we provide tools for <br /> professional services
+              delivery and operational <br /> excellence that ensures our client
+              satisfaction <br /> and value for our shareholders.
+            </p>
+          </div>
+          <div className="sellingPoints">
+            <h5>OUR UNIQUE SELLING POINTS</h5>
+            <div className="sellingPointsText">
+              <div className="sellingPointsCol1">
+                {sellingPoints1.map((point, index) => (
+                  <ul key={index} className="points">
+                    <li>
+                      <span className="icon">{point.icon}</span>
+                      <span className="text">{point.text}</span>
+                    </li>
+                  </ul>
+                ))}
+              </div>
+              <div className="sellingPointsCol2">
+                {sellingPoints2.map((point, index) => (
+                  <ul key={index} className="points">
+                    <li>
+                      <span className="icon">{point.icon}</span>
+                      <span className="text">{point.text}</span>
+                    </li>
+                  </ul>
+                ))}
+              </div>
+            </div>
+          </div>
+        </article>
+        <article className="overlay"></article>
+      </section>
+      <section className="indexFifthSection">
+        <h5 className="text-center">Certifications and Licenses</h5>
+        <div className="certificates">
+          {certification.map((certificate, index) => (
+            <div key={index}>
+              <img src={certificate.certification} alt="certification" />
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="indexSixthSection">
+        <h4 className="text-center">SAFETY</h4>
+        <h5 className="text-center"> Our SHE-Q Policy</h5>
+        <article className="safety__policy">
+          <div className="safety__policy-text">
+            <p>
+              Our Safety, Health, Environment and Quality (SHE-Q) culture aims
+              for an <br /> incident-free working environment because safety
+              management is a priority <br /> to us.
+            </p>
+            <p>
+              We conduct a continuous review, internal audit and development of
+              our <br /> SHE-Q policies and procedures to ensure that the
+              highest standards are <br /> met at all times.
+            </p>
+            <p>
+              We have implemented several in-house safety policies in line with
+              our <br /> Safety Management Systems which includes; our safety
+              policy, security <br /> policy, alcohol and drug policy to mention
+              a few.
+            </p>
+            <p>
+              Our seasoned in-house safety officers/managers have increased the
+              safety <br /> awareness and inculcated a safety culture in all
+              staff and contractors of <br /> SR PLATFORMS LIMITED.
+            </p>
+          </div>
+          <div className="safety__policy-text">
+            <p>
+              Our safety leadership campaigns are based on sound safety values
+              and <br />
+              rules designed to make people aware of their own responsibilities{" "}
+              <br />
+              regarding safety.
+            </p>
+            <p>
+              We encourage them to take action if operations appear unsafe and
+              to <br />
+              approach others if they feel at risk.
+            </p>
+            <p>
+              Health and safety risks differ from project to project and from
+              location to <br /> location, so it is essential to have the right
+              tools at hand to assess them, <br /> take appropriate measures and
+              communicate the solutions to all involved.
+            </p>
+            <p>
+              Our Management Systems set clear standards that emphasize exactly
+              what <br /> we expect from our Team with regard to safety, health,
+              consideration for <br /> the environment and the quality of what
+              we deliver as a team.
+            </p>
+          </div>
+        </article>
+        <div className="safety__section">
+          <img src={require("../assets/img/safetyimage.png")} alt="safety" />
+          <h5>SAFETY FIRST BECAUSE INJURY LASTS!!!.</h5>
+        </div>
+      </section>
+
+      <section className="indexSeventhSection">
+        {gallery.map((item, index) => (
+          <div className="gallery__item" key={index}>
+            <img src={item.gallery} alt="gallery" />
+            <div className="overlay"></div>
+          </div>
+        ))}
+      </section>
+
+      <section className="indexEightSection">
+        <h5 className="text-center">A Few of our Clients</h5>
+        <div className="clients">
+          {clients.map((client, index) => (
+            <div key={index}>
+              <img src={client.client} alt="client" />
+            </div>
+          ))}
         </div>
       </section>
     </Layout>
