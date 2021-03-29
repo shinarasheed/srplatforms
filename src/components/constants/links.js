cf7d3f40c1894dc8bb484b9b254f16a846aa48e4
shina <rasheedshinaopeyemi@gmail.com>
2021-03-29 17:18:22 +0100

    added compliance to the sidebar

--- src/components/constants/links.js
+++ src/components/constants/links.js
@@ -37,7 +37,7 @@ const Links = ({ styleClass }) => {
       </li>
       <li>
         <Link to="/compliance" className="nav__list-link">
-          Compliance
+          compliance
         </Link>
       </li>
       <li>
