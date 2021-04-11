20db2febb1bd1639084887ba4c1270b7889cea10
shina <rasheedshinaopeyemi@gmail.com>
2021-04-11 22:23:35 +0100

    added more seo specific functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,11 @@ module.exports = {
     description: `Foremost Indigenous Company with Expertise in Marine/Offshore Support Services to the oil & Gas Industry Using Innovative service Delivery`,
     author: `gravitytech`,
     keywords: `
-    SR PLatforms, SR, SR Platform,SR Platform Limited, Oil and Gas, oil and gas companies in Nigeria,oil and gas companies in Lagos, Nigeria,oil and gas companies in lagos hiring, nigeria oil & gas companies, lagos oil & gas companies,vessel security, oil and gas security, marine security, marine vessels, vessel protection, oil and gas security company, vessels security company, marine escorts,
+    SR PLatforms, SR, SR Platform, SR Platform Limited, Offshore Supply Vessels, Offshore Support, Maritime Security, Maritime Security Nigeria, Maritime security & logistics company, Maritime Security company, Security Vessels in Nigeria, Marine Security, Security Vessel, Marine Excort Vessels, Private Security Company, Private Marine Security Company, Marine Security Comapny in Nigeria, Marine Security Vessels, Offshore Security, Offshore Security Vessels, Offshore Security Vessels, Marine Security Company, Oil and Gas, oil and gas companies in Nigeria,oil and gas companies in Lagos, Nigeria,oil and gas companies in lagos hiring, nigeria oil & gas companies, lagos oil & gas companies,vessel security, oil and gas security, marine security, marine vessels, vessel protection, oil and gas security company, vessels security company, marine escorts,
     southwest oil & gas companies, oil and gas safety bill, Marine,  vessels , offshore, marine vessels , offshore vessels , marine operations , drilling and production , upstream , oil servicing , petroleum , offshore support, fsiv , psv, ahts , Lhts, csv, crew boats , tug boats, surfer boats,  security boats ,security vessels , platform supports , anchor handling`,
     twitterusername: "iamrasheedshina",
     image: "/static/bg5.png",
-    siteUrl: "https://www.srplatforms.com/",
+    siteUrl: "https://www.srplatforms.com",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
