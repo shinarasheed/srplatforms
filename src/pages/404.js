74afbdfd340d7a58ca08e7b7ff5be2f145290137
shinarasheed <rasheedshinaopeyemi@gmail.com>
2023-08-07 01:38:55 +0100

    removed npm

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,6 @@ import SeoComponent from "../components/SeoComponent";
 const NotFoundPage = () => (
   <Layout>
     <SeoComponent title="404: Not found" />
-    <h5>404: Not Found</h5>
     <p>Page Not Found</p>
   </Layout>
 );
